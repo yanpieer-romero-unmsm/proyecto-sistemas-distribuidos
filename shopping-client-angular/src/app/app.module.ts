04e01952994a852060c612f1fd44914caca6ccda
Miguel Armas <miguel.armas@unmsm.edu.pe>
2021-09-05 16:39:36 -0500

    master alert pagar

--- shopping-client-angular/src/app/app.module.ts
+++ shopping-client-angular/src/app/app.module.ts
@@ -5,6 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { OrderrequestComponent } from './components/orderrequest/orderrequest.component';
 import { HttpClientModule } from '@angular/common/http';
+import { NavbarComponent } from './utils/navbar/navbar.component';
+import { UtilsModule } from './utils/utils.module';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
   declarations: [
@@ -14,7 +19,11 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    UtilsModule,
+    NoopAnimationsModule,
+    FormsModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- shopping-client-angular/src/app/components/orderrequest/orderrequest.component.ts
+++ shopping-client-angular/src/app/components/orderrequest/orderrequest.component.ts
@@ -8,13 +8,35 @@ import { ArticlesService } from '../../services/articles.service';
   styleUrls: ['./orderrequest.component.css']
 })
 export class OrderrequestComponent implements OnInit {
+  ruc: string = "";
   articles: Article[];
+  quantity: number[] = [];
 
   constructor(private articleService: ArticlesService) { }
 
   ngOnInit(): void {
     this.articleService.listar()
     .subscribe(articles => this.articles = articles);
+  }
+
+  isDisabled(stock: number, indexArray: number): boolean {
+    var num: number = Number(this.quantity[indexArray-1]);
+    if(stock<num){
+      console.log("TRUE------------------");
+      return true;
+    }
+    console.log("FALSE------------------");
+    return false;
+  }
+
+  pagar(idArticle: string, stock: number, indexArray: number): void {
+    var available: number = Number(this.quantity[indexArray]);
+    if(stock<available) {
+      window.alert("No hay la cantidad solicitada");
+    } else {
+      console.log("Se realizó el pago");
+    }
+
   }
 
 }
