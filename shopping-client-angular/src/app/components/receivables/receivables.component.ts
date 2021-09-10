21615f6a5899d56c6eb7c431ee8177078c40f1c0
yanpieer-romero-unmsm <yanpieer.romero@unmsm.edu.pe>
2021-09-09 23:09:36 -0500

    Creando componente receivables

---
+++ shopping-client-angular/src/app/components/receivables/receivables.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-receivables',
+  templateUrl: './receivables.component.html',
+  styleUrls: ['./receivables.component.css']
+})
+export class ReceivablesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
